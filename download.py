0567a97e5df41b1647dc6be76373204f263d72e4
Yue Wu <wuyuejl97@gmail.com>
2021-12-20 00:11:32 -0800

    Basic photo download

--- download.py
+++ download.py
@@ -1,57 +1,101 @@
-import requests
-import os
 import json
+import requests
+import shutil
 
-# To set your enviornment variables in your terminal run the following line:
-# export 'BEARER_TOKEN'='<your_bearer_token>'
-bearer_token = os.environ.get('BEARER_TOKEN')
+from pathlib import Path
 
 
-def create_url():
-    # Tweet fields are adjustable.
-    # Options include:
-    # attachments, author_id, context_annotations,
-    # conversation_id, created_at, entities, geo, id,
-    # in_reply_to_user_id, lang, non_public_metrics, organic_metrics,
-    # possibly_sensitive, promoted_metrics, public_metrics, referenced_tweets,
-    # source, text, and withheld
-    tweet_fields = 'tweet.fields=lang,author_id'
-    # Be sure to replace your-user-id with your own user ID or one of an authenticating user
-    # You can find a user ID by using the user lookup endpoint
-    id = 'your-user-id'
+with open('info.json') as f:
+    info = json.load(f)
+
+username = info['username']
+bearer_token = info['bearer_token']
+out_dir = info['out']
+
+def create_url(id):
+    params = {
+        'expansions': 'attachments.media_keys',
+        'media.fields': 'url'
+    }
     # You can adjust ids to include a single Tweets.
     # Or you can add to up to 100 comma-separated IDs
-    url = 'https://api.twitter.com/2/users/{}/liked_tweets'.format(id)
-    return url, tweet_fields
+    url = f'https://api.twitter.com/2/users/{id}/liked_tweets'
+    return url, params
 
 
 def bearer_oauth(r):
-    """
-    Method required by bearer token authentication.
-    """
-
     r.headers['Authorization'] = f'Bearer {bearer_token}'
     r.headers['User-Agent'] = 'v2LikedTweetsPython'
     return r
 
 
-def connect_to_endpoint(url, tweet_fields):
-    response = requests.request(
-        'GET', url, auth=bearer_oauth, params=tweet_fields)
-    print(response.status_code)
-    if response.status_code != 200:
-        raise Exception(
-            'Request returned an error: {} {}'.format(
-                response.status_code, response.text
-            )
-        )
-    return response.json()
+def verify_resp(resp: requests.Response):
+    if resp.status_code != 200:
+        raise Exception(f'Request returned an error: {resp.status_code} {resp.text}')
+
+
+def get_user_id(username):
+    resp = requests.get(f'https://api.twitter.com/2/users/by/username/{username}', auth=bearer_oauth)
+    verify_resp(resp)
+    data = resp.json()
+    return data['data']['id']
+    
+
+def get_likes(url, params):
+    resp = requests.get(url, auth=bearer_oauth, params=params)
+    verify_resp(resp)
+    data = resp.json()
+
+    out_path = Path(out_dir)
+
+    while data['meta']['result_count'] > 0:
+        # data['data'] contains the tweets and media keys.
+        # data['includes'] contains the urls of medias.
+        # Establish a correspondence of tweets and media urls.
+
+        # Dictionary between keys and urls
+        media_dict: dict[str, str] = {}
+        for media in data['includes']['media']:
+            if media['type'] == 'photo':
+                media_dict[media['media_key']] = media['url']
+            else:
+                # Currently Twitter API v2 doesn't provide video download links. A hack will be used.
+                # https://stackoverflow.com/questions/32145166
+                pass
+
+
+
+        for like in data['data']:
+            tweet_path: Path = out_path / like['id']
+            tweet_path.mkdir(exist_ok=True)
+
+            text_path: Path = tweet_path / 'text.txt'
+            with text_path.open('wt') as f:
+                f.write(like['text'])
+            
+            if 'attachments' in like:
+                for media_key in like['attachments']['media_keys']:
+                    if media_key in media_dict:
+                        url = media_dict[media_key]
+                        extension = url[url.rfind('.'):]
+                        resp = requests.get(url, stream=True)
+                        verify_resp(resp)
+                        resp.raw.decode_content = True
+                        image_path = tweet_path / (media_key + extension)
+                        with image_path.open('wb') as f:
+                            shutil.copyfileobj(resp.raw, f)
+                    # TODO: handle video above
+
+        if 'next_token' in data['meta']:
+            resp = requests.get(url, auth=bearer_oauth, params=params | {'pagination_token': data['meta']['next_token']})
+        else:
+            break
 
 
 def main():
-    url, tweet_fields = create_url()
-    json_response = connect_to_endpoint(url, tweet_fields)
-    print(json.dumps(json_response, indent=4, sort_keys=True))
+    user_id = get_user_id(username)
+    url, params = create_url(user_id)
+    get_likes(url, params)
 
 
 if __name__ == '__main__':
